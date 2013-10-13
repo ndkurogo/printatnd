dcc295aaef3151a3484c850786ec6949f0d95fc3
Kevin Riehm <kevin.riehm@gmail.com>
2013-10-13 00:04:59 -0400

    Give a printer id separate from the friendly name

--- config/initializers/printers.rb
+++ config/initializers/printers.rb
@@ -1,12 +1,12 @@
 configuration_file = YAML.load(File.open(File.expand_path("../../printers.yml", __FILE__)))
 
-queues = configuration_file.inject({}) do |memo, (k, v)|
-  memo.merge(k => v[:queue])
-end
-
 printers = configuration_file.inject([]) do |memo, (k, v)|
   memo << k
 end
 
+printernames = configuration_file.inject({}) do |memo, (k, v)|
+  memo.merge(k => v[:id])
+end
+
 $printers = printers
-$queues = queues
+$printernames = printernames
