40b105af5583b55172f7741af8070a8dacbde3ed
Kevin Riehm <kevin.riehm@gmail.com>
2013-11-15 19:51:57 -0500

    Whitelisted .txt and .rtf files

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-DIRECT_EXTENSIONS = %w(.pdf .eps .ps)
-GOOGLE_EXTENSIONS = %w(.odt .ods .doc .docx .xls .xlsx .ppt .pptx .pages .ai .psd .tiff .dxf .svg)
+DIRECT_EXTENSIONS = %w(.pdf .eps .ps .txt)
+GOOGLE_EXTENSIONS = %w(.odt .ods .doc .docx .xls .xlsx .ppt .pptx .pages .ai .psd .tiff .dxf .svg .rtf)
 
 ALL_EXTENSIONS = DIRECT_EXTENSIONS + GOOGLE_EXTENSIONS
