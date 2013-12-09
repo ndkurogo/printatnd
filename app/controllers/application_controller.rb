b23a3751e229b73c8d91d98f87e66be1789a31c3
Kevin Riehm <kevin.riehm@gmail.com>
2013-12-09 14:13:41 -0500

    Removed remnants of CU's printer system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_globals
 
   def set_globals
-    gon.printers = $printers
-    gon.status = $redis.hgetall("status")
   end
 end
 
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@ r = Redis.new
 Resque.redis = r
 Resque.redis.namespace = "printatnd:resque"
 Resque.inline = Rails.env.development?
-Resque.schedule = YAML.load_file(Rails.root.join("config/schedule.yml"))
 $redis = Redis::Namespace.new("printatnd", :redis => r)
+
