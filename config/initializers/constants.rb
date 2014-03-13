d0ebb983eccfc1da896737c3ea3952981423df90
Kevin Riehm <kevin.riehm@gmail.com>
2014-03-13 13:57:21 -0500

    Disabled JPEG uploading

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-IMAGE_EXTENSIONS = %w(.gif .jpg .jpeg .png)
+IMAGE_EXTENSIONS = %w(.gif .png)
 DIRECT_EXTENSIONS = %w(.pdf .eps .ps .txt) + IMAGE_EXTENSIONS
 GOOGLE_EXTENSIONS = %w(.odt .ods .doc .docx .xls .xlsx .ppt .pptx .pages .ai .psd .tiff .dxf .svg .rtf)
 
