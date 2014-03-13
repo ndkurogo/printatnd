1eb8b5a779dd44eed03a9b40e98abd7d01e34fbe
Kevin Riehm <kevin.riehm@gmail.com>
2014-03-13 14:05:00 -0500

    Disabled .dxf and .pages uploading

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 IMAGE_EXTENSIONS = %w(.gif .png)
 DIRECT_EXTENSIONS = %w(.pdf .eps .ps .txt) + IMAGE_EXTENSIONS
-GOOGLE_EXTENSIONS = %w(.odt .ods .doc .docx .xls .xlsx .ppt .pptx .pages .ai .psd .tiff .dxf .svg .rtf)
+GOOGLE_EXTENSIONS = %w(.odt .ods .doc .docx .xls .xlsx .ppt .pptx .ai .psd .tiff .svg .rtf)
 
 ALL_EXTENSIONS = DIRECT_EXTENSIONS + GOOGLE_EXTENSIONS
